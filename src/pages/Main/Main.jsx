b66f9f3c307d46224aceb462b0d9e2aa1dc218de
jocker-crocker <jocker.crocker@gmail.com>
2025-02-02 18:05:28 +0400

    git init

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import NewsBanner from "../../components/NewsBanner/NewsBanner";
+import style from "./styles.module.css";
+import { getNews } from "../../api/apiNews";
+import NewsList from "../../components/NewsList/NewsList";
+
+const Main = () => {
+    const [news, setNews] = useState([])
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await getNews();
+        setNews(response.news);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchNews();
+  }, []);
+
+  return (
+    <main className={style.main}>
+      {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+
+      <NewsList news={news} />
+    </main>
+  );
+};
+
+export default Main;
